c13821573d45f7009a6760376a1efefc8c7bdcaf
Cenooo <94233626+Cenooo@users.noreply.github.com>
2022-01-27 14:12:39 +0100

    Add files via upload

---
+++ ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cepolat <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/27 13:17:53 by cepolat           #+#    #+#             */
+/*   Updated: 2022/01/27 13:21:04 by cepolat          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+# define EVEN 
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+
+typedef int		t_bool;
+
+#endif
